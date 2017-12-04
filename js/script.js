49b50093dba1d2923c89f550b01217eb3bf29740
joe mckinney <mckinney.a.joe@gmail.com>
2017-12-03 22:58:02 -0800

    add functionality to the page

---
+++ js/script.js
@@ -0,0 +1,44 @@
+function Ticket(movie, time, age) {
+  this.movie = movie;
+  this.time = time;
+  this.age = age;
+}
+
+Ticket.prototype.viewTicket = function(){
+  return "Movie: " + this.movie + " Time: " + this.time + " Age: " + this.age;
+}
+
+Ticket.prototype.ticketCost = function() {
+  var cost = 0;
+  if ((this.movie === "Dazed and Confused") || (this.movie === "Indiana Jones and the Last Crusade") || (this.movie === "Rosemary's Baby") || (this.movie === "I got the Hook-up")) {
+    cost += 5;
+  } if (this.age === "Adult") {
+    cost += 3;
+  } if (this.age === "child") {
+    cost += 1;
+  } if ((this.time === "2:00 PM") || (this.time === "5:00 PM")) {
+    cost += 2;
+  } if ((this.time === "7:00 PM") || (this.time === "9:00 PM")) {
+    cost += 4;
+  }
+  return cost;
+
+}
+
+$(document).ready(function() {
+  $("#userInput").submit(function(event) {
+    event.preventDefault();
+    var movie = $("#movie").val();
+    var age = $("#age").val();
+    var time = $("#time").val();
+    var newTicket = new Ticket(movie, time, age);
+    var ticketFace = newTicket.viewTicket();
+    var ticketTotal = newTicket.ticketCost();
+    console.log(ticketFace);
+    console.log(ticketTotal);
+
+    $(".movie").text(movie);
+    $(".time").text(time);
+    $(".age").text(age);
+  });
+});
