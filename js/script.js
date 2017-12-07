c965f68fa116ebd40337e8d8426038a2af320872
joe mckinney <mckinney.a.joe@gmail.com>
2017-12-07 12:36:42 -0800

    add script to make the checkboxes correlate with a cost for each snack and then it will add the snack price to the ticket price for a total price

--- js/script.js
+++ js/script.js
@@ -1,12 +1,32 @@
-function Ticket(movie, time, age) {
+
+function Ticket(movie, time, age, snacks) {
   this.movie = movie;
   this.time = time;
   this.age = age;
+  this.snacks = snacks;
 }
 
-// Ticket.prototype.viewTicket = function(){
-//   return "Movie: " + this.movie + " Time: " + this.time + " Age: " + this.age;
-// }
+Ticket.prototype.food = function(){
+    var money = 0;
+    if (this.snacks.length === 1) {
+      money += 2;
+    } else if (this.snacks.length === 2) {
+      money += 4;
+    } else if (this.snacks.length === 3) {
+      money += 6;
+    } else if (this.snacks.length === 4) {
+      money += 8;
+    } else if (this.snacks.length === 5) {
+      money += 10;
+    } else if (this.snacks.length === 6) {
+      money += 12;
+    } else if (this.snacks.length === 7) {
+      money += 14;
+    } else if (this.snacks.length === 8) {
+      money += 16;
+    }
+    return money;
+}
 
 Ticket.prototype.ticketCost = function() {
   var cost = 0;
@@ -26,19 +46,30 @@ Ticket.prototype.ticketCost = function() {
 }
 
 $(document).ready(function() {
+  var snacks = [];
   $("#userInput").submit(function(event) {
     event.preventDefault();
     var movie = $("#movie").val();
     var age = $("#age").val();
     var time = $("#time").val();
-    var newTicket = new Ticket(movie, time, age);
+    var newTicket = new Ticket(movie, time, age, snacks);
+    $("input:checkbox[name=treats]:checked").each(function() {
+      snacks.push($(this).val());
+    });
+    console.log(snacks);
     // var ticketFace = newTicket.viewTicket();
     var ticketTotal = newTicket.ticketCost();
+    var snackMoney = newTicket.food();
+    console.log(snackMoney);
     $("#form-div").hide();
     $(".movie").text(movie);
     $(".time").text(time);
     $(".age").text(age);
     $(".cost").text(ticketTotal);
+    $(".treats").text(snackMoney);
+    var total = snackMoney += ticketTotal;
+    console.log(total);
+    $(".total").text(total);
     $("#ticket").show(8000);
   });
 });
